27f5f709e9197bc5340f32b5387e0578a52f5148
Saral-Chauhangit <saralchauhan424t@gmail.com>
2025-05-05 10:59:52 +0530

    Refactor authentication configuration and middleware setup. Added 'admin' and 'guest' middleware aliases in app.php, and updated auth.php to use static values for guard, passwords, and added support for 'admins' provider.

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Middleware\Authenticate;
+use App\Middleware\RedirectIfAuthenticated;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
@@ -11,7 +13,10 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        //
+        $middleware->alias([
+            'admin' => Authenticate::class,
+            'guest' => RedirectIfAuthenticated::class
+        ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
